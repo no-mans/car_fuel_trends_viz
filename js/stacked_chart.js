38edea540496502f7d94e677a7621c1c9dea31ec
noam.mansovsky <noam.mansovsky@zoominfo.com>
2021-08-01 22:37:00 +0300

    put some space in x axis ticks

--- js/stacked_chart.js
+++ js/stacked_chart.js
@@ -215,7 +215,10 @@ async function init() {
 
     svg.append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickSizeOuter(0));
+    .call(d3.axisBottom(x)
+        .tickSizeOuter(0)
+        .tickValues(x.domain().filter(function(d,i){ return !(i%2)}))
+    );
 
     svg.append("text")
       .attr("transform",
@@ -228,7 +231,7 @@ async function init() {
     var formatPercent = d3.format(".0%");
 
     var y = d3.scaleLinear()
-    .domain([0, 1])
+    .domain([0, 0.6])
     .range([ height, 0 ]);
     svg.append("g")
     .call(d3.axisLeft(y).tickFormat(formatPercent));
